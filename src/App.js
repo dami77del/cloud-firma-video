aa343a06cd02348c6caca410c06179dafa9cfb53
dami77del <ddeligdisch@gmail.com>
2023-05-04 00:48:05 +0200

    add componets About + support

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
-
+import About from "./components/About";
+import Support from "./components/Support";
 function App() {
   return (
     <>
       <NavBar />
       <Hero/>
+    <About/>
+    <Support/>
 
     </>
   );
